83f4dc669c6a118c77072045ed317a7b28bc75a6
allanstar-byte <allanmutisya263@gmail.com>
2022-07-25 10:46:26 +0300

    user addons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/user";
 import Interests from "./Interests";
 
-function Profile({ user, theme }) {
-  if (!user) return <h2>Please Login To View Profile</h2>;
-  return (
-    <div>
-      <h2>{user.name}'s Profile</h2>
-      <Interests interests={user.interests} theme={theme} />
-    </div>
-  );
+function Profile() {
+    const { user } = useContext(UserContext);
+    if (!user) return <h2 > Please Login To View Profile < /h2>;
+    return ( <
+        div >
+        <
+        h2 > { user.name }
+        's Profile</h2> <
+        Interests interests = { user.interests }
+        /> <
+        /div>
+    );
 }
 
 export default Profile;
